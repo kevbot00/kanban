73934f051016de726f5f7c8d5a8de86cf499c854
Kevin Yang <kyang812@Yahoo.com>
2026-08-21 09:32:24 -1000

    create a get board/:id endpoint

--- api/src/boards/boards.controller.ts
+++ api/src/boards/boards.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+  constructor(private readonly boardsService: BoardsService) {}
+
   @Get('/:id')
-  getBoardById(@Param('id') id: string): { id: string } {
-    return { id };
+  async getBoardById(@Param('id') id: string): Promise<any> {
+    return await this.boardsService.findById(id);
   }
 }
--- api/src/boards/boards.service.ts
+++ api/src/boards/boards.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string): Promise<any> {
+    const board = await this.prisma.board.findUnique({
+      where: { id },
+      include: {
+        columns: {
+          orderBy: { position: 'asc' },
+          omit: { createdAt: true, updatedAt: true },
+          include: {
+            cards: {
+              orderBy: { position: 'asc' },
+              omit: { createdAt: true, updatedAt: true },
+            },
+          },
+        },
+      },
+    });
+
+    if (!board) return null;
+
+    return board;
+  }
+}
