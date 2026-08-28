7d6e5f96e1d974e9d28fde7d4748a0c925973fad
Kevin Yang <kyang812@Yahoo.com>
2026-08-28 10:10:40 -1000

    create a gitlab-ci

--- e2e/support/config.ts
+++ e2e/support/config.ts
@@ -5,6 +5,7 @@ export const API_URL = `http://localhost:${API_PORT}`;
 export const WEB_URL = `http://localhost:${WEB_PORT}`;
 
 export const TEST_DATABASE_URL =
+  process.env.TEST_DATABASE_URL ??
   'postgresql://kanban:kanban@localhost:5432/kanban_test?schema=public';
 
 export { SEEDED_BOARD_ID } from '../../api/prisma/seed-data';
